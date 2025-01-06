4da4aa6ce8d9564c4a0a2bbcdd35702a850336a0
Asprofil <adober.demchenko@gmail.com>
2025-01-06 23:20:20 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
-require('dotenv').config()
-const { initMongoConnection } = require('./db/initMongoConnection')
-const { setupServer } = require('./server')
+// Завантажуємо змінні оточення з .env файлу
+require('dotenv').config();
 
+// Імпортуємо функцію для підключення до MongoDB
+const { initMongoConnection } = require('./db/initMongoConnection');
+
+// Імпортуємо Express
+const express = require('express');
+
+// Ініціалізація Express додатку
+const app = express();
+
+// Налаштовуємо парсинг JSON у запитах
+app.use(express.json());
+
+// Функція для запуску сервера
 const startApp = async () => {
-  await initMongoConnection()
-  setupServer()
-}
+  // Підключаємося до MongoDB
+  await initMongoConnection();
 
-startApp()
+  // Визначаємо простий маршрут
+  app.get('/', (req, res) => {
+    res.send('Hello, MongoDB!');
+  });
+
+  // Запускаємо сервер на порті 3000
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+// Запуск додатку
+startApp();
