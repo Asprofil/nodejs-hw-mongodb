f4f01cd5aadc6c37078cda6f0405b1911f03e78c
Asprofil <adober.demchenko@gmail.com>
2025-01-07 21:19:47 +0200

    Update contacts.js

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const { getContacts, getContactById, createContact, updateContact, deleteContact } = require('../controllers/contacts');
+const { ctrlWrapper } = require('../utils/ctrlWrapper');
 const validateBody = require('../middlewares/validateBody');
 const isValidId = require('../middlewares/isValidId');
 const { contactSchema, updateContactSchema } = require('../models/contactValidation');
 
-router.get('/', getContacts);
-router.get('/:contactId', isValidId, getContactById);
-router.post('/', validateBody(contactSchema), createContact);
-router.patch('/:contactId', isValidId, validateBody(updateContactSchema), updateContact);
-router.delete('/:contactId', isValidId, deleteContact);
+router.get('/', ctrlWrapper(getContacts));
+router.get('/:contactId', isValidId, ctrlWrapper(getContactById));
+router.post('/', validateBody(contactSchema), ctrlWrapper(createContact));
+router.patch('/:contactId', isValidId, validateBody(updateContactSchema), ctrlWrapper(updateContact));
+router.delete('/:contactId', isValidId, ctrlWrapper(deleteContact));
 
 module.exports = router;
